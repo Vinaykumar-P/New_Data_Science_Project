ed5022fcc127ba34eb31e2fbd0c78a5dc4086dc1
Vinaykumar Pandey <asopensky@gmail.com>
2025-09-01 00:54:10 +0530

    Update Myapp.py

--- Myapp.py
+++ Myapp.py
@@ -1,17 +1,20 @@
 import yfinance as yf
 import streamlit as st
+from datetime import datetime
 import pandas as pd
 
 st.write("""
          
-        ## Simple Stock Tracking app
-        # Congratulations Vinay you did it!
+        # Simple Stock Tracking app
+        ### Congratulations Vinay you did it!
      
         """)
+# Get current date and time
+
 
 tS='HDFCBANK.NS'
 tD= yf.Ticker(tS)
 #tDF=tD.history(start='2010-5-31', end='2020-5-31')
-tDF = tD.history(start='2020-05-31', end='2025-08-29')
+tDF = tD.history(start='2022-04-01')
 st.line_chart(tDF.Close)
 st.line_chart(tDF.Volume)
